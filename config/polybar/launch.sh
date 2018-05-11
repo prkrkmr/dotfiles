4af3f56c44ec8ed5e99f736895f2b7a84e061332
prkrkmr <prkrkmr@gmail.com>
2018-05-11 22:56:48 +0530

    Changed configuration

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,13 +1,19 @@
-#!/usr/bin/env sh
+i#!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q polybar
+# killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top & polybar bottom
+#polybar base &
 
+sleep .5
 
-echo "Bars launched..."
+if ! pgrep -x polybar; then
+        polybar base &
+    else
+        pkill -USR1 polybar
+    fi
+        echo "Bars launched..."
---
+++ copy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+cp ~/.mpd/ ~/random/dotfiles/ -rv
+cp ~/.ncmpcpp/ ~/random/dotfiles/ -rv
+cp ~/.config/i3/ ~/random/dotfiles/config/ -rv
+cp ~/.config/rofi/ ~/random/dotfiles/config/ -rv
+cp ~/.config/ranger/ ~/random/dotfiles/config/ -rv
+cp ~/.config/polybar/ ~/random/dotfiles/config/ -rv
